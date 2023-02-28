6db1c807d42db7303bec64e44f4655d4f1d40117
JareJarekus <yaros7553@google.com>
2023-02-28 18:07:12 +0200

    first commit

---
+++ js/file.js
@@ -0,0 +1,12 @@
+const bodyElement = document.body;
+
+const someChild = bodyElement.firstElementChild;
+console.log(someChild);
+//-----------------
+const childNodes = bodyElement.childNodes;
+console.log(childNodes);
+// ----------------
+const div = document.querySelector('.megaClass');
+console.log(div);
+const div0 = document.getElementById('0');
+console.log(div0);
